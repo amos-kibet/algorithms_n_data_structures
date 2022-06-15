b22fa43523fa792ffe0d10ec1aa4e2f077297ba1
amos-kibet <kibetamos97@gmail.com>
2022-06-15 21:56:20 +0300

    Add solution to HackerRank's 'Add Two Ints' challenge

---
+++ add_two.py
@@ -0,0 +1,23 @@
+"""
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of  and 
+"""
+
+from cv2 import add
+
+
+def add_two(a,b):
+	# Hint: Type return a+b below
+  parsed_a = int(a)
+  parsed_b = int(b)
+  print(parsed_a + parsed_b)
+
+add_two(2, 9)
